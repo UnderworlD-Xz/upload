09e257bec5fb97e3ecd4db589d18bc44d0d28ff0
UnderworlD-Xz <123926592+UnderworlD-Xz@users.noreply.github.com>
2024-11-14 09:48:36 +0700

    Update index.php

--- index.php
+++ index.php
@@ -33,6 +33,7 @@ if (isset($_GET['delete'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>File List</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
